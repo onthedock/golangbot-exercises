10b8fe403df51c39e6e4d82bbaf704340752fdcd
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 15:31:43 +0000

    Multiple return values

--- 05-functions/main.go
+++ 05-functions/main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
-func calculateBill(price, no int) int {
-	var totalPrice = price * no
-	return totalPrice
+func rectProps(length, width float64) (float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter
 }
 
 func main() {
-	price, no := 90, 6
-	totalPrice := calculateBill(price, no)
-	fmt.Println("Total price is", totalPrice)
+	area, perimeter := rectProps(10.8, 5.6)
+	fmt.Printf("Area %f Perimeter %f\n", area, perimeter)
 }
