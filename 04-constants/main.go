c58fdc11a739312578831be5b267c37384771172
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 10:57:21 +0000

    Constant values must be known at compile time: the value of a constant cannot be assigned from the return value of a function (the value will only be known at runtime)

--- 04-constants/main.go
+++ 04-constants/main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
 func main() {
-	const (
-		name  = "Xavi"
-		age   = 123
-		color = "Green"
-	)
-	fmt.Println("My name is", name, "and I am", age, "years old. I like the color", color)
-	color = "Red" // Error: the value of a constant cannot be changed: it's constant ;)
+	var a = math.Sqrt(4)   // allowed
+	const b = math.Sqrt(4) // not allowed
 }
