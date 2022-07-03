5a59cec358ca30a463415ff7829db122b5e4210c
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 16:36:51 +0000

    Idiomatic 'if' statement - avoid innecessary branching

--- 08-if-else/main.go
+++ 08-if-else/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	if num := 10; num%2 == 0 { // 'num' is only available inside the if...else statement
+	num := 10
+	// Idiomatic Go avoids innecessary branching and indentation of code.
+	// Also, return as early as possible.
+	if num%2 == 0 {
 		fmt.Println(num, "is even")
-	} else {
-		fmt.Println(num, "is odd")
+		return
 	}
+	fmt.Println(num, "is odd")
 }
