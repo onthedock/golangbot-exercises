970424bd4ef5751cb5c39d2a2c6359ce245e555f
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:41:21 +0000

    Expressionless switch

--- 10-switch/main.go
+++ 10-switch/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	letter := "i"
-	fmt.Printf("Letter %s is a ", letter)
-	switch letter {
-	case "a", "e", "i", "o", "u": // multiple expresions in case are allowed
-		fmt.Println("vowel")
+	number := 75
+	switch { // the switch is considered 'true'
+	case number >= 0 && number < 50: // each condition in the case statement is evaluated
+		fmt.Printf("%d is greater or equal than 0, but less than 50\n", number)
+	case number >= 50 && number < 100:
+		fmt.Printf("%d is greater or equal than 50, but less than 100\n", number)
 	default:
-		fmt.Println("not a vowel")
+		fmt.Printf("%d is greater than 100\n", number)
 	}
 }
