9081a8dfe09da2e806efc21c7abd1a178d79f1e7
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:16:16 +0000

    Infinite loop

--- 09-loops/main.go
+++ 09-loops/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	for number, index := 10, 1; index <= 10 && number <= 19; index, number = index+1, number+1 { //multiple initialisation and increment
-		fmt.Printf("%d * %d = %d\n", number, index, number*index)
+	for { // infinite loop
+		fmt.Println("Are we there yet?...")
 	}
 }
