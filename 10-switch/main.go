6d90d46424eaffb25061ce108e2f8fd94839ab83
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:26:47 +0000

    'switch' statement

---
+++ 10-switch/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+const msg string = `A switch is a conditional statement that evaluates an expression and compares it
+against a list of possible matches and executes the corresponding block of code.
+It can be considered as an idiomatic way of replacing complex "if...else" clauses.
+`
+
+func main() {
+	fmt.Println(msg)
+
+	finger := 3
+	fmt.Printf("Finger %d is ... ", finger)
+
+	switch finger {
+	case 1:
+		fmt.Println("Thumb")
+	case 2:
+		fmt.Println("Index")
+	case 3:
+		fmt.Println("Middle")
+	case 4:
+		fmt.Println("Ring")
+	case 5:
+		fmt.Println("Pinky")
+	}
+}
