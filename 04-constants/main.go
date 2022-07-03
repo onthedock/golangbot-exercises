7b3012bb0860ac151baac34f46f516c354135fb9
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 10:52:09 +0000

    Declaring multipe constants at once

--- 04-constants/main.go
+++ 04-constants/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const a = 50
-	fmt.Println("a =", a)
+	const (
+		name  = "Xavi"
+		age   = 123
+		color = "Green"
+	)
+	fmt.Println("My name is", name, "and I am", age, "years old. I like the color", color)
 }
