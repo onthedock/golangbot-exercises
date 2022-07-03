ce33d0a41194b24607660cc89229bbaaf8cacc91
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:14:28 +0000

    Multiple initialisation and post

--- 09-loops/main.go
+++ 09-loops/main.go
@@ -5,9 +5,7 @@ import (
 )
 
 func main() {
-	i := 0
-	for i <= 10 { // initialisation and post are omitted
-		fmt.Printf("%d ", i)
-		i += 3
+	for number, index := 10, 1; index <= 10 && number <= 19; index, number = index+1, number+1 { //multiple initialisation and increment
+		fmt.Printf("%d * %d = %d\n", number, index, number*index)
 	}
 }
