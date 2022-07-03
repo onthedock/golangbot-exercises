c017f55f45f634f74eef6a53633ccf8c51677ebf
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 16:58:10 +0000

    break terminates the loop abruptly (the program continues after the 'for' block)

--- 09-loops/main.go
+++ 09-loops/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 10; i++ {
+		if i > 5 {
+			break // the loop is terminated
+			// execution continues after the 'for' loop
+		}
 		fmt.Printf("%d ", i)
 	}
+	fmt.Println("\n(break ends the loop)\nafter the loop")
 }
