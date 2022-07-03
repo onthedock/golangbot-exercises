5698b531edda2203dd94084896033e01c11abd10
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 09:24:54 +0000

    Multiple variables can be declared at once

--- 02-variables/main.go
+++ 02-variables/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var age = 22
-	fmt.Println("My age is", age)
+	var width, heigh int = 20, 40
+	fmt.Println("'width' is", width, "and 'height' is", heigh)
 }
