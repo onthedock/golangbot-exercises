96cc1cef3bb153e0afe2eb8abc2d29893e4e8f8e
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:33:44 +0000

    Multiple expressions in a 'case', separated by commas

--- 10-switch/main.go
+++ 10-switch/main.go
@@ -2,29 +2,13 @@ package main
 
 import "fmt"
 
-const msg string = `A switch is a conditional statement that evaluates an expression and compares it
-against a list of possible matches and executes the corresponding block of code.
-It can be considered as an idiomatic way of replacing complex "if...else" clauses.
-`
-
 func main() {
-	fmt.Println(msg)
-
-	finger := 30
-	fmt.Printf("Finger %d is ... ", finger)
-
-	switch finger {
-	case 1:
-		fmt.Println("Thumb")
-	case 2:
-		fmt.Println("Index")
-	case 3:
-		fmt.Println("Middle")
-	case 4:
-		fmt.Println("Ring")
-	case 5:
-		fmt.Println("Pinky")
+	letter := "i"
+	fmt.Printf("Letter %s is a ", letter)
+	switch letter {
+	case "a", "e", "i", "o", "u": // multiple expresions in case are allowed
+		fmt.Println("vowel")
 	default:
-		fmt.Println("Invalid finger number")
+		fmt.Println("not a vowel")
 	}
 }
