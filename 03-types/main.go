b1ee7e0f246ff39aa64e3246a52d996961e5a5b2
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 10:16:04 +0000

    Explicit conversion (from float64 to int)

--- 03-types/main.go
+++ 03-types/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	i := 3       // int
-	j := 2.3     // float
-	sum := i + j // ERROR: mismatched types int and float64
-	fmt.Println(sum)
+	i := 3            // int
+	j := 2.3          // float
+	sum := i + int(j) // Explicit conversion to type 'int'
+	fmt.Println(sum)  // prints 5
 }
