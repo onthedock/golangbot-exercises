fa39c9a022cf3ba489f9db9b00316f5b1670395b
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 09:38:42 +0000

    (error) 'No new variables on the left of :='. Short hand declaration can only be used when at least one the variables on the left side of := is newly declared

--- 02-variables/main.go
+++ 02-variables/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	name, age := "Xavi"
+	name, age := "Xavi", 123
+	fmt.Println("My name is", name, "and I am", age, "years old")
+	name, age := "Lisa", 321
 	fmt.Println("My name is", name, "and I am", age, "years old")
 }
