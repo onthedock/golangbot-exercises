b6df3ea5b2fa9d6c78f312c01b2d1c0407d5580b
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 17:54:07 +0000

    Fallthrough happens even when the case evaluates to false!

--- 10-switch/main.go
+++ 10-switch/main.go
@@ -4,20 +4,12 @@ import (
 	"fmt"
 )
 
-func number() int {
-	num := 15 * 5 // num = 75
-	return num
-}
-
 func main() {
-	switch num := number(); { //num is not a constant
-	case num < 50: // this evaluates to true
+	switch num := 25; {
+	case num < 50:
 		fmt.Printf("%d is lesser than 50\n", num)
-		fallthrough
-	case num < 100: // this results in 'true'
-		fmt.Printf("%d is lesser than 100\n", num) // print the message
-		fallthrough                                // continue evaluating 'case' statements
-	case num < 200: // it is not checked (because of the previous fallthrough)!!!
-		fmt.Printf("%d is lesser than 200\n", num) // print the message
+		fallthrough // When fallthrough is encountered the control moves to the first statement of the next case
+	case num > 100: // skipped because of the fallthrough!!!
+		fmt.Printf("%d is greater than 100\n", num) // this is printed!!!
 	}
 }
