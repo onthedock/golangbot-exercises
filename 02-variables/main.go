05492f1099af60286a4bf67a2d55a88eaa34865b
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-03 09:33:19 +0000

    Short hand declaration (one variable)

--- 02-variables/main.go
+++ 02-variables/main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		name   = "Xavi"
-		age    = "123"
-		height int
-	)
-	fmt.Println("My name is", name, "and I am", age, "years old. My current height is", height)
+	count := 10
+	fmt.Println("Count =", count)
 }
