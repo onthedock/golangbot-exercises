a5bb8a3b48215f2af78824b3813985e84c43b6d5
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:16:52 +0200

    Using len() and a 'for' loop to iterate through all elements of the array

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	a := [...]float64{67.7, 89.8, 21, 78}
-	fmt.Println("length of a is", len(a))
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("The %dth element of the array is %.2f\n", i, a[i])
+	}
 
 }
