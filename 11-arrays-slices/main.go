e6e49767e92076cd281da6deac689398fb1e65b8
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:11:50 +0200

    Arrays are value types (and they are passed as values to the function, so the original remains unchanged)

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -2,15 +2,14 @@ package main
 
 import "fmt"
 
+func changeLocal(num [5]int) {
+	num[0] = 55
+	fmt.Println("inside function ", num)
+
+}
 func main() {
-	// Arrays in Go are value types and not reference types.
-	// This means that when they are assigned to a new variable,
-	// a copy of the original array is assigned to the new variable.
-	// If changes are made to the new variable, it will not be
-	// reflected in the original array.
-	a := [...]string{"USA", "China", "India", "Germany", "France"}
-	b := a // a copy of a is assigned to b
-	b[0] = "Singapore"
-	fmt.Println("a is ", a)
-	fmt.Println("b is ", b)
+	num := [...]int{5, 6, 7, 8, 8}
+	fmt.Println("before passing to function ", num)
+	changeLocal(num) //num is passed by value
+	fmt.Println("after passing to function ", num)
 }
