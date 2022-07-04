cbab2bf61ceb01aff31ac516af99765fd5d987f9
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:45:51 +0200

    The 'zero' value for slices is 'nil'

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	cars := []string{"Ferrari", "Honda", "Ford"}
-	fmt.Println("cars:", cars, "has old length", len(cars), "and capacity", cap(cars)) //capacity of cars is 3
-	cars = append(cars, "Toyota")
-	fmt.Println("cars:", cars, "has new length", len(cars), "and capacity", cap(cars)) //capacity of cars is doubled to 6
+	var names []string //zero value of a slice is nil
+	if names == nil {
+		fmt.Println("slice is nil going to append")
+		names = append(names, "John", "Sebastian", "Vinay")
+		fmt.Println("names contents:", names)
+	}
 }
