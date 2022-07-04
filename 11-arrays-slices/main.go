2afda1105bd7b5fb62b9edfff27517ac0a237f10
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:44:15 +0200

    Appending new values to slices

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
-	i := make([]int, 5, 5)
-	fmt.Println(i)
+	cars := []string{"Ferrari", "Honda", "Ford"}
+	fmt.Println("cars:", cars, "has old length", len(cars), "and capacity", cap(cars)) //capacity of cars is 3
+	cars = append(cars, "Toyota")
+	fmt.Println("cars:", cars, "has new length", len(cars), "and capacity", cap(cars)) //capacity of cars is doubled to 6
 }
