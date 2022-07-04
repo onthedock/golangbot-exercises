04c5e371d70798909a839c991ddf22d9a6e40b1b
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:47:55 +0200

    Passing an slice to a function

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
-func main() {
-	var names []string //zero value of a slice is nil
-	if names == nil {
-		fmt.Println("slice is nil going to append")
-		names = append(names, "John", "Sebastian", "Vinay")
-		fmt.Println("names contents:", names)
+func subtactOne(numbers []int) {
+	for i := range numbers {
+		numbers[i] -= 2
 	}
+
+}
+func main() {
+	nos := []int{8, 7, 6}
+	fmt.Println("slice before function call", nos)
+	subtactOne(nos)                               //function modifies the slice
+	fmt.Println("slice after function call", nos) //modifications are visible outside
 }
