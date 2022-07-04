a083a997b45ada290471633c0dfb074e2b14e3e9
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:40:10 +0200

    func make([]T, len, cap) []T can be used to create a slice by passing the type, length and capacity. The capacity parameter is optional and defaults to the length. The make function creates an array and returns a slice reference to it.

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -5,12 +5,6 @@ import (
 )
 
 func main() {
-	numa := [3]int{78, 79, 80}
-	nums1 := numa[:] //creates a slice which contains all elements of the array
-	nums2 := numa[:]
-	fmt.Println("array before change 1", numa)
-	nums1[0] = 100
-	fmt.Println("array after modification to slice nums1", numa)
-	nums2[1] = 101
-	fmt.Println("array after modification to slice nums2", numa)
+	i := make([]int, 5, 5)
+	fmt.Println(i)
 }
