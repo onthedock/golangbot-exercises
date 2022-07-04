2f0c407a7c910442a84d54cd4d357527f9f26a10
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:13:50 +0200

    len(<array>) returns the arrays's length

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -2,14 +2,8 @@ package main
 
 import "fmt"
 
-func changeLocal(num [5]int) {
-	num[0] = 55
-	fmt.Println("inside function ", num)
-
-}
 func main() {
-	num := [...]int{5, 6, 7, 8, 8}
-	fmt.Println("before passing to function ", num)
-	changeLocal(num) //num is passed by value
-	fmt.Println("after passing to function ", num)
+	a := [...]float64{67.7, 89.8, 21, 78}
+	fmt.Println("length of a is", len(a))
+
 }
