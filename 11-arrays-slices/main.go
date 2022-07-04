c9758b7cd811a07f4a5de86cf08759f5a4b00bbe
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:21:28 +0200

    Using range() and a 'for' loop to go over all the elements of the array

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	a := [...]float64{67.7, 89.8, 21, 78}
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("The %dth element of the array is %.2f\n", i, a[i])
-	}
+	var sum float64 = 0
 
+	for n, v := range a { // range returns the index and the value of the array
+		fmt.Printf("The %dth element of the array is %.2f\n", n, v)
+		sum += v
+	}
+	fmt.Printf("The sum of all elements is %.2f\n", sum)
 }
