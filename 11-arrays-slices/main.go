2ad042d0954ddc7ad7d1456eb18ef40bda7e2765
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:27:36 +0200

    Slices are like arrays, but with variable length. They are declared as []Type

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := [...]float64{67.7, 89.8, 21, 78}
-	var sum float64 = 0
-
-	for n, v := range a { // range returns the index and the value of the array
-		fmt.Printf("The %dth element of the array is %.2f\n", n, v)
-		sum += v
-	}
-	fmt.Printf("The sum of all elements is %.2f\n", sum)
+	a := []int{12, 21, 32, 48, 95}
+	var b []int = a[1:3] // gets items from the element a[1] to the element befon a[3]=a[3-1]=a[2]
+	fmt.Println(b)
 }
