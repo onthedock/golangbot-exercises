60bb276ccf470f838cbb8b47496df6bbdf44223b
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:07:14 +0200

    The size of the array is a part of the type

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -1,8 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := [...]int{12, 78, 50}
-	fmt.Println(a)
+	a := [3]int{12, 78, 50}
+	var b [5]int
+	b = a // Error: The size of the array is a part of the type; therefore, they are different types and 'a' cannot be assigned to 'b'
 }
