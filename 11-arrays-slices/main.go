6fd546cd49e411328e6051b2a7916299b5242a4a
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-04 19:29:48 +0200

    []Type{v1, v2...} creates an array and returns an slice reference

--- 11-arrays-slices/main.go
+++ 11-arrays-slices/main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{12, 21, 32, 48, 95}
-	var b []int = a[1:3] // gets items from the element a[1] to the element befon a[3]=a[3-1]=a[2]
-	fmt.Println(b)
+	a := []int{12, 21, 32, 48, 95} // Creates an array and returns an slice reference
+	fmt.Println(a)
 }
