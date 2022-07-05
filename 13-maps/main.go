f39b6e3022784ccf4d66b075f2cd69340976fc0c
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 19:10:49 +0200

    Deleting items from a map

--- 13-maps/main.go
+++ 13-maps/main.go
@@ -9,7 +9,7 @@ func main() {
 		"Marc":    45,
 		"Anthony": 55,
 	}
-	for k, v := range employeeAge {
-		fmt.Printf("The employee '%s' is '%d' years old\n", k, v)
-	}
+	fmt.Println("Initial map:", employeeAge)
+	delete(employeeAge, "Marc")
+	fmt.Println("Final map:", employeeAge)
 }
