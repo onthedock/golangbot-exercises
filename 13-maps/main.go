7af1ddd6cc65f9251549d1302f66e37ede6b726d
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 19:20:36 +0200

    Map of structs

--- 13-maps/main.go
+++ 13-maps/main.go
@@ -2,14 +2,22 @@ package main
 
 import "fmt"
 
+type employee struct {
+	country string
+	age     int
+}
+
 func main() {
-	employeeAge := map[string]int{
-		"John":    33,
-		"Jane":    32,
-		"Marc":    45,
-		"Anthony": 55,
+	e1 := employee{"USA", 21}
+	e2 := employee{"Maxico", 19}
+	e3 := employee{"France", 22}
+	employeeInfo := map[string]employee{
+		"Mick":     e1,
+		"Miguel":   e2,
+		"Delphine": e3,
 	}
-	fmt.Println("Initial map:", employeeAge)
-	delete(employeeAge, "Marc")
-	fmt.Println("Final map:", employeeAge)
+
+	for name, info := range employeeInfo {
+		fmt.Printf("Employee: '%s' is '%d' years old from '%s'\n", name, info.age, info.country)
+	}
 }
