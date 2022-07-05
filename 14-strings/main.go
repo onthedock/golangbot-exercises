84b986f4d8745982c2625f41c143e9f0d1d00f4e
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 19:54:54 +0200

    Seems ok to print a character using %c, but it's a bug

--- 14-strings/main.go
+++ 14-strings/main.go
@@ -7,12 +7,25 @@ import (
 func printBytes(s string) {
 	fmt.Printf("Bytes: ")
 	for i := 0; i < len(s); i++ {
-		fmt.Printf("%x ", s[i]) // %x is the format specifier for hexadecimal
+		fmt.Printf("%x ", s[i])
+	}
+}
+
+func printChars(s string) { // This function contains a BUG!!!!!
+	fmt.Printf("Characters: ")
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%c ", s[i]) // %c format specifier is used to print the characters of the string
 	}
 }
 
 func main() {
 	name := "Hello World"
-	fmt.Printf("String: %s\n", name) // %s is the format specifier to print a string
-	printBytes(name)                 // Prints --> Bytes: 48 65 6c 6c 6f 20 57 6f 72 6c 64
+	fmt.Printf("String: %s\n", name)
+	printChars(name)
+	fmt.Printf("\n")
+	printBytes(name)
+	// Output:
+	// String: Hello World
+	// Characters: H e l l o   W o r l d
+	// Bytes: 48 65 6c 6c 6f 20 57 6f 72 6c 64
 }
