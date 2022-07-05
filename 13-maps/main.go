717e2bf23d378b684346c9c3e5bd30c67158bfc6
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 18:47:16 +0200

    Initialize values during definition and adding more values later

--- 13-maps/main.go
+++ 13-maps/main.go
@@ -6,7 +6,7 @@ func main() {
 	employeeAge := map[string]int{
 		"John": 33,
 		"Jane": 32,
-		"Ann":  21,
 	}
+	employeeAge["Ann"] = 21
 	fmt.Println(employeeAge) // prints -> map[Ann:21 Jane:32 John:33]
 }
