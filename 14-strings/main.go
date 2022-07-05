7c76d37b330cb71f6eb89cb2253233fb0410a821
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 20:07:41 +0200

    Creating a string from a slice of bytes

--- 14-strings/main.go
+++ 14-strings/main.go
@@ -4,20 +4,11 @@ import (
 	"fmt"
 )
 
-func charsAndBytePosition(s string) {
-	for index, rune := range s { // returns the position of the byte where the rune starts along with the rune
-		fmt.Printf("%c starts at byte %d\n", rune, index)
-	}
-}
-
 func main() {
-	name := "Señor"
-	charsAndBytePosition(name)
+	byteSlice := []byte{0x43, 0x61, 0x66, 0xC3, 0xA9}
+	str := string(byteSlice)
+	fmt.Println(str)
 }
 
 // Output:
-// S starts at byte 0 <-- 1 byte
-// e starts at byte 1 <-- 1 byte
-// ñ starts at byte 2 <-- 2 bytes (2 and 3) are used by 'ñ'
-// o starts at byte 4 ...
-// r starts at byte 5
+// Café
