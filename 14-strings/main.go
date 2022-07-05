91d935d426fdee2be64b8b3a18624c99ebab0d69
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 19:38:56 +0200

    Definition of 'string' in Go

---
+++ 14-strings/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "A string is a slice of bytes in Go. Strings can be created by enclosing a set of characters inside double quotes \" \"."
+	fmt.Println(s)
+}
