1a88bac2e45d44022639e0e89b2f2e34f8eb255e
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-05 18:45:26 +0200

    Initialize a map during declaration

--- 13-maps/main.go
+++ 13-maps/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	employeeAge := make(map[string]int)
-	employeeAge["John"] = 33
-	employeeAge["Jane"] = 32
-	employeeAge["Ann"] = 21
+	employeeAge := map[string]int{
+		"John": 33,
+		"Jane": 32,
+		"Ann":  21,
+	}
 	fmt.Println(employeeAge) // prints -> map[Ann:21 Jane:32 John:33]
 }
