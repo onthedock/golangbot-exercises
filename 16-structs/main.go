f2d9a6d92a9c5dd5d06c34d33781bb370674c6fe
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-10 20:47:25 +0200

    Creating struct variables (with or without specifying field names)

--- 16-structs/main.go
+++ 16-structs/main.go
@@ -1,6 +1,22 @@
 package main
 
+import "fmt"
+
 type Employee struct {
-	firstName, lastName string
-	age                 int
+	firstName string
+	lastName  string
+	age       int
+}
+
+func main() {
+	// creates a struct specifying field names
+	emp1 := Employee{
+		firstName: "Sam",
+		age:       25,
+		lastName:  "Anderson",
+	}
+	// creates a struct without specifying field names
+	emp2 := Employee{"Federico", "Mercurio", 33}
+	fmt.Println("Employee 1", emp1)
+	fmt.Println("Employee 2", emp2)
 }
