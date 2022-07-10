daaaf5ac06c03a3e81f809eca9b90dbe0af301ca
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-10 20:15:33 +0200

    The 'new()' function creates pointers too. The 'new()' function takes a type as argument and returns a pointer to a 'zero' value for the type passed as argument

--- 15-pointers/main.go
+++ 15-pointers/main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	a := 25
-	var b *int
-	if b == nil {
-		fmt.Println("b is", b)
-		b = &a
-		fmt.Println("b after initialization is", b)
-	}
+	size := new(int)
+	fmt.Printf("Size value is %d, type is %T and address is %v\n", *size, size, size)
+	*size = 85
+	fmt.Println("New 'size' value is", *size)
 }
