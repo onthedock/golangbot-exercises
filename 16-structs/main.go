7d25cc94e8471ddf201faccc70ce1ef81ce57a82
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-10 20:54:33 +0200

    It is possible to initialize some fields but not all of them; those not given an explicit value, get the 'zero' value for its type

--- 16-structs/main.go
+++ 16-structs/main.go
@@ -9,10 +9,10 @@ type Employee struct {
 }
 
 func main() {
-	var emp1 Employee // Not initialized -> zero values
+	var emp1 Employee
+	emp1.firstName = "John"
+	emp1.lastName = "Doe"
 	fmt.Println("First name", emp1.firstName)
 	fmt.Println("Last name", emp1.lastName)
 	fmt.Println("Age", emp1.age)
-	emp1.firstName = "Samuel"
-	fmt.Println("New name", emp1.firstName)
 }
