3b881732ee544ff9e848d6d6f91c005acf8c4195
Xavi Aznar <xavi.aznar@outlook.com>
2022-07-10 20:11:34 +0200

    The 'zero' value of a pointer is 'nil'

--- 15-pointers/main.go
+++ 15-pointers/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	b := 255
-	var a *int = &b
-	fmt.Printf("Type of a is %T\n", a)
-	fmt.Println("address of b is", a)
+	a := 25
+	var b *int
+	if b == nil {
+		fmt.Println("b is", b)
+		b = &a
+		fmt.Println("b after initialization is", b)
+	}
 }
